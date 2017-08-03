b7ff8e790f0422ec100c8049f1b1c353cb0cb97e
wujiawei <wujiawei@xiaomi.com>
2017-08-03 13:37:04 +0800

    Update setup
    Signed-off-by: wujiawei <wujiawei@xiaomi.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description="douban album downloader",
     url="https://github.com/einverne/douban-dl",
     author="einverne",
-    email="einverne@gmail.com",
+    author_email="einverne@gmail.com",
     license="MIT",
     # scripts=["bin/douban-album-dl"],
     # not use scripts while use entry_points instead https://packaging.python.org/tutorials/distributing-packages/#scripts
@@ -22,7 +22,7 @@ setup(
         'console_scripts': [
             'douban_dl = douban.__main__:main',
         ]},
-    keywords="douban dl",
+    keywords="douban downloader",
     packages=find_packages(exclude=["tests"]),
     install_requires=requirements,
 )
